c437958a85bae625e88d4058839e776d3e6c0078
Shashank Jain <159939097+01Shashank01@users.noreply.github.com>
2025-04-11 13:37:34 +0530

    Update model_pipeline.py

--- model_pipeline.py
+++ model_pipeline.py
@@ -17,7 +17,7 @@ def run_model(stock_name, interval, window):
 
 
     # Initialize Alpha Vantage API
-    API_KEY = "JA8LGGRXPBABCFOA"
+    API_KEY = "LXPW27XHTU181BV3"
     ts = TimeSeries(key=API_KEY, output_format="pandas")
 
     def fetch_stock_data(ts, symbol, interval):
